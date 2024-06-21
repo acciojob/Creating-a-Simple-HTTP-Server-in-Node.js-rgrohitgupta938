e59f1f7a8ffeda4a315d1b671a9c6c35118fa136
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-21 18:14:39 +0530

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, world!");
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server is listening on port ${port}`);
 });
 
 // DO NOT EDIT BELOW THIS LINE
 
-module.exports = { server }
+module.exports = { server };
